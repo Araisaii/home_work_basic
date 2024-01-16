64b90bbc4904f45fff101784e57f4addcc96cff7
Araisaii <95917732+Araisaii@users.noreply.github.com>
2024-01-16 16:01:50 +0300

    Revert "Hw03 chessboard"

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	var x, y int
-	fmt.Print("Введите ширину и длину доски: ")
-	fmt.Scan(&x, &y)
-	for i := 1; i <= y; i++ {
-		for j := 1; j <= x; j++ {
-			switch {
-			case (i%2 != 0 && j%2 != 0) || (i%2 == 0 && j%2 == 0):
-				fmt.Print("#")
-			case (i%2 != 0 && j%2 == 0) || (i%2 == 0 && j%2 != 0):
-				fmt.Print(" ")
-			}
-		}
-		fmt.Println()
-	}
+	// Place your code here.
 }
