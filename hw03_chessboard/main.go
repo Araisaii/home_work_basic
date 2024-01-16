3a5525e2af0188fc1ee6bcfba165ebd501750d87
Anna Arays <anyta_arays@mail.ru>
2024-01-16 16:37:38 +0300

    HW03 chessboard

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Place your code here.
+	var x, y int
+	fmt.Print("Введите ширину и длину доски: ")
+	fmt.Scan(&x, &y)
+	for i := 1; i <= y; i++ {
+		for j := 1; j <= x; j++ {
+			switch {
+			case (i%2 != 0 && j%2 != 0) || (i%2 == 0 && j%2 == 0):
+				fmt.Print("#")
+			case (i%2 != 0 && j%2 == 0) || (i%2 == 0 && j%2 != 0):
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
 }
