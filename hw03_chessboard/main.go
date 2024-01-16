a0a4a7afd043af962733cb4281ee24f244acb3d8
Anna Arays <anyta_arays@mail.ru>
2024-01-16 15:55:15 +0300

    corrected code

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -8,26 +8,15 @@ func main() {
 	var x, y int
 	fmt.Print("Введите ширину и длину доски: ")
 	fmt.Scan(&x, &y)
-
 	for i := 1; i <= y; i++ {
-		if i%2 != 0 {
-			for j := 1; j <= x; j++ {
-				if j%2 != 0 {
-					fmt.Print("#")
-				} else {
-					fmt.Print(" ")
-				}
+		for j := 1; j <= x; j++ {
+			switch {
+			case (i%2 != 0 && j%2 != 0) || (i%2 == 0 && j%2 == 0):
+				fmt.Print("#")
+			case (i%2 != 0 && j%2 == 0) || (i%2 == 0 && j%2 != 0):
+				fmt.Print(" ")
 			}
-			fmt.Println()
-		} else {
-			for j := 1; j <= x; j++ {
-				if j%2 != 0 {
-					fmt.Print(" ")
-				} else {
-					fmt.Print("#")
-				}
-			}
-			fmt.Println()
 		}
+		fmt.Println()
 	}
 }
